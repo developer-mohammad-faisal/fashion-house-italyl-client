23df1341c1f93cb26682c7dedadf8e6a97903f74
nasirdiu <nasir.joypurhat@gmail.com>
2022-06-19 23:28:11 +0600

    aaa

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div>Footer
+    <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius error voluptatem, consequuntur recusandae cum optio officia itaque repellendus cumque vero corrupti iusto nisi, pariatur corporis necessitatibus, asperiores aliquid provident obcaecati?</h1>
+  </div>;
 };
 
 export default Footer;
