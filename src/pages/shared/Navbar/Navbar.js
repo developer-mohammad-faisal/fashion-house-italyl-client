88c592782d601a96ae7e3bcbacb06d187df1fb91
Your Name <you@example.com>
2022-06-19 22:01:57 +0600

    navbar 80% done

--- src/pages/shared/Navbar/Navbar.js
+++ src/pages/shared/Navbar/Navbar.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import {
   faBagShopping,
   faEnvelope,
@@ -11,22 +12,27 @@ import logo from "../../../assets/images/logo/logo.webp";
 const Navbar = () => {
   return (
     <Fragment>
-      <section className="">
+      <section>
+        {/*phone, email and My Account top navbar */}
         <div className="flex px-5 lg:px-10 justify-between  items-center">
           <div className="hidden lg:inline-flex gap-x-5">
             <div>
-              <FontAwesomeIcon icon={faPhone} /> <span >+8801998839126</span>
+              <FontAwesomeIcon icon={faPhone} /> <span>+8801998839126</span>
             </div>
             <div>
-              <FontAwesomeIcon icon={faEnvelope} /> <span className="hover:cursor-pointer hover:primary-300" > fasion.house.italy@gmail.com </span>
+              <FontAwesomeIcon icon={faEnvelope} />{" "}
+              <span className="hover:cursor-pointer hover:primary-300">
+                {" "}
+                fasion.house.italy@gmail.com{" "}
+              </span>
             </div>
-            </div>
+          </div>
           <div>
-            <div class="dropdown hover:cursor-pointer dropdown-hover">
-              <h1 class="flex justify-between items-center">
+            <div className="dropdown hover:cursor-pointer dropdown-hover">
+              <h1 className="flex justify-between items-center">
                 My Account
                 <svg
-                  class="fill-current"
+                  className="fill-current"
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
                   height="20"
@@ -36,8 +42,8 @@ const Navbar = () => {
                 </svg>
               </h1>
               <ul
-                tabindex="0"
-                class="dropdown-content menu p-3 shadow bg-base-100 rounded-box w-44"
+                tabIndex="0"
+                className="dropdown-content menu p-3 shadow bg-base-100 rounded-box w-44"
               >
                 <li>
                   <a>Login </a>
@@ -50,177 +56,130 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div class="navbar bg-base-300 px-5 my-3 lg:px-10">
-          <div class="navbar-start">
-            <div class="dropdown">
-              <label tabindex="0" class="btn btn-ghost lg:hidden">
+        {/* logo, catagories, search and cart icon option second navbar  */}
+        <div className="navbar bg-info px-5 my-3 py-6 lg:px-10">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <label tabIndex="0" className="btn mt-16 btn-ghost lg:hidden">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5"
+                  className="h-5 w-5"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M4 6h16M4 12h8m-8 6h16"
                   />
                 </svg>
               </label>
               <ul
-                tabindex="0"
-                class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+                tabIndex="0"
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
               >
-                <li tabindex="0">
-                  <a class="justify-between">
-                    All Categories
-                    <svg
-                      class="fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                    </svg>
-                  </a>
-                  <ul class="p-2 border-2 bg-base-300">
-                    <li>
-                      <a>High Heels</a>
-                    </li>
-                    <li>
-                      <a>Flat Slippers</a>
-                    </li>
-                    <li>
-                      <a>Hand Bags</a>
-                    </li>
-                    <li>
-                      <a>Modern Tops</a>
-                    </li>
-                    <li>
-                      <a>Fittops</a>
-                    </li>
-                    <li>
-                      <a>T-shirt</a>
-                    </li>
-                    <li>
-                      <a>Jeans</a>
-                    </li>
-                  </ul>
-                </li>
+                <select defaultValue="All Categories" className="select">
+                  <option value="All Categories"  disabled selected>
+                  All Categories
+                  </option>
+                  <option value="High Heels" >High Heels</option>
+                  <option value="Flat Slippers" >Flat Slippers</option>
+                  <option value="Hand Bags" >Hand Bags</option>
+                  <option value="Modern Tops" >Modern Tops</option>
+                  <option value="Fittops" >Fittops</option>
+                  <option value="T-shirt" >T-shirt</option>
+                  <option value="Jeans" >Jeans</option>
+                </select>
               </ul>
             </div>
-            <a class="btn btn-ghost normal-case text-xl">
+            <a className="btn btn-ghost normal-case text-xl">
               {" "}
-              <img src={logo} alt="" />{" "}
+              <img className="" src={logo} alt="" />{" "}
             </a>
           </div>
-          <div class="navbar-center hidden lg:flex">
-            <ul class="menu menu-horizontal p-0">
-              <li tabindex="0">
-                <a>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal p-0">
+              <select defaultValue='All Categories' className="select">
+                <option value="All Categories"  disabled selected>
                   All Categories
-                  <svg
-                    class="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                  </svg>
-                </a>
-                <ul class="p-2 border-2 bg-base-300">
-                  <li>
-                    <a>High Heels</a>
-                  </li>
+                </option>
+                <option value="High Heels" >High Heels</option>
+                <option value="Flat Slippers" >Flat Slippers</option>
+                <option value="Hand Bags" >Hand Bags</option>
+                <option value="Modern Tops" >Modern Tops</option>
+                <option value="Fittops" >Fittops</option>
+                <option value="T-shirt" >T-shirt</option>
+                <option value="Jeans" >Jeans</option>
+              </select>
 
-                  <li>
-                    <a>Flat Slippers</a>
-                  </li>
-                  <li>
-                    <a>Hand Bags</a>
-                  </li>
-                  <li>
-                    <a>Modern Tops</a>
-                  </li>
-                  <li>
-                    <a>Fittops</a>
-                  </li>
-                  <li>
-                    <a>T-shirt</a>
-                  </li>
-                  <li>
-                    <a>Jeans</a>
-                  </li>
-                </ul>
-              </li>
               <input
                 type="text"
                 placeholder="Search.."
-                class="rounded-md input my-3 input-group-lg h-full"
+                className="input input-group-lg ml-3"
               />
-              <button class="btn my-3 btn-ghost btn-circle">
+              <button className="btn btn-ghost btn-circle">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-7 w-7"
+                  className="h-7 w-7"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                   />
                 </svg>
               </button>
             </ul>
           </div>
-          <div class="navbar-end gap-x-4 ">
+          <div className="navbar-end mt-16 lg:mt-0 gap-x-4">
             <div className="text-4xl">
               <FontAwesomeIcon icon={faHeart} />
             </div>
-            <div className="text-4xl flex items-center">
+            <div className="indicator text-4xl flex items-center">
+              <span className="indicator-item badge badge-secondary">0</span>
               <FontAwesomeIcon icon={faBagShopping} />
             </div>
           </div>
         </div>
 
-        <div class="navbar flex-col justify-center items-center px-5 lg:px-10">
-          <div class="navbar-start">
-            <div class="dropdown">
-              <label tabindex="0" class="btn btn-ghost lg:hidden">
+        {/* all catagories product name center third navbar */}
+        <div className="navbar flex-col justify-center items-center px-5 lg:px-10">
+          <div className="navbar-start">
+            <div className="hidden lg:dropdown">
+              <label tabIndex="0" className="btn btn-ghost lg:hidden">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5"
+                  className="h-5 w-5"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M4 6h16M4 12h8m-8 6h16"
                   />
                 </svg>
               </label>
               <ul
-                tabindex="0"
-                class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+                tabIndex="0"
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
               >
                 <li>
-                  <a>Item 1</a>
+                  <a>Home</a>
                 </li>
-                <li tabindex="0">
-                  <a class="justify-between">
-                    Parent
+                <li tabIndex="0">
+                  <a className="justify-between">
+                    Collection
                     <svg
-                      class="fill-current"
+                      className="fill-current"
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
                       height="24"
@@ -229,7 +188,7 @@ const Navbar = () => {
                       <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
                     </svg>
                   </a>
-                  <ul class="p-2">
+                  <ul className="p-2">
                     <li>
                       <a>Submenu 1</a>
                     </li>
@@ -242,16 +201,16 @@ const Navbar = () => {
             </div>
           </div>
 
-          <div class="navbar-center  hidden lg:flex">
-            <ul class=" menu menu-horizontal p-0">
+          <div className="navbar-center  hidden lg:flex">
+            <ul className=" menu menu-horizontal p-0">
               <li>
                 <a>Home</a>
               </li>
-              <li tabindex="0">
+              <li tabIndex="0">
                 <a>
                   Collection
                   <svg
-                    class="fill-current"
+                    className="fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     width="20"
                     height="20"
@@ -260,7 +219,7 @@ const Navbar = () => {
                     <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
                   </svg>
                 </a>
-                <ul class="p-2">
+                <ul className="p-2">
                   <li>
                     <a>Submenu 1</a>
                   </li>
@@ -269,11 +228,11 @@ const Navbar = () => {
                   </li>
                 </ul>
               </li>
-              <li tabindex="0">
+              <li tabIndex="0">
                 <a>
                   Shop
                   <svg
-                    class="fill-current"
+                    className="fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     width="20"
                     height="20"
@@ -282,7 +241,7 @@ const Navbar = () => {
                     <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
                   </svg>
                 </a>
-                <ul class="p-2">
+                <ul className="p-2">
                   <li>
                     <a>Submenu 1</a>
                   </li>
@@ -291,11 +250,11 @@ const Navbar = () => {
                   </li>
                 </ul>
               </li>
-              <li tabindex="0">
+              <li tabIndex="0">
                 <a>
                   Trendy Wear
                   <svg
-                    class="fill-current"
+                    className="fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     width="20"
                     height="20"
@@ -304,7 +263,7 @@ const Navbar = () => {
                     <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
                   </svg>
                 </a>
-                <ul class="p-2">
+                <ul className="p-2">
                   <li>
                     <a>Submenu 1</a>
                   </li>
@@ -313,11 +272,11 @@ const Navbar = () => {
                   </li>
                 </ul>
               </li>
-              <li tabindex="0">
+              <li tabIndex="0">
                 <a>
                   Sandals
                   <svg
-                    class="fill-current"
+                    className="fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     width="20"
                     height="20"
@@ -326,7 +285,7 @@ const Navbar = () => {
                     <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
                   </svg>
                 </a>
-                <ul class="p-2">
+                <ul className="p-2">
                   <li>
                     <a>Submenu 1</a>
                   </li>
@@ -335,11 +294,11 @@ const Navbar = () => {
                   </li>
                 </ul>
               </li>
-              <li tabindex="0">
+              <li tabIndex="0">
                 <a>
                   Accessories
                   <svg
-                    class="fill-current"
+                    className="fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     width="20"
                     height="20"
@@ -348,7 +307,7 @@ const Navbar = () => {
                     <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
                   </svg>
                 </a>
-                <ul class="p-2">
+                <ul className="p-2">
                   <li>
                     <a>Submenu 1</a>
                   </li>
@@ -357,11 +316,11 @@ const Navbar = () => {
                   </li>
                 </ul>
               </li>
-              <li tabindex="0">
+              <li tabIndex="0">
                 <a>
                   Pages
                   <svg
-                    class="fill-current"
+                    className="fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     width="20"
                     height="20"
@@ -370,7 +329,7 @@ const Navbar = () => {
                     <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
                   </svg>
                 </a>
-                <ul class="p-2">
+                <ul className="p-2">
                   <li>
                     <a>About</a>
                   </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,25 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      primary: {
-        50: "#ffeff3",
-        100: "#ffe0e9",
-        200: "#ffc6d9",
-        300: "#ff97b9",
-        400: "#ff5d95",
-        500: "#ff2476",
-        600: "#ff0069",
-        700: "#d70058",
-        800: "#b40053",
-        900: "#99024d",
-      },
-    },
     extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#EE156A",
+
+          secondary: "#F7F7F7",
+  
+          accent: "#ffff",
+  
+          neutral: "#3D4451",
+  
+          info: "#000000",
+        },
+      },
+      "dark",
+    ],
   },
   plugins: [require("daisyui")],
 };
