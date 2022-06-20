69376c648d9c909de0ddfbe3e2c920866ab2e4c9
Your Name <you@example.com>
2022-06-21 02:57:03 +0600

    feature section done

--- src/StaticData/ProductData.js
+++ src/StaticData/ProductData.js
@@ -8,23 +8,68 @@ export const ProductData = [
   },
 ];
 
+// Banner Data
 export const bannerData = [
   {
     id: 1,
+    img: 'https://i.ibb.co/hgdtbqr/banner1.webp',
     title: "New Collections",
     name: "Women's Fashion",
     des: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi dignissimos blanditiis enim ipsum aliquam ipsam, velit voluptatibus aperiam suscipit vel amet sunt corrupti facere odio eaque ad nobis impedit exercitationem. ",
   },
   {
     id: 2,
+    img: 'https://i.ibb.co/DrXVJCc/banner3.webp',
     title: "New Arrivals",
     name: "Accessories",
     des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi dignissimos blanditiis enim ipsum aliquam ipsam, velit voluptatibus aperiam suscipit vel amet sunt corrupti facere odio eaque ad nobis impedit exercitationem.",
   },
   {
     id: 3,
+    img: 'https://i.ibb.co/XxRcPGk/banner2.webp',
     title: "Top Trending",
     name: "Clothings",
     des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi dignissimos blanditiis enim ipsum aliquam ipsam, velit voluptatibus aperiam suscipit vel amet sunt corrupti facere odio eaque ad nobis impedit exercitationem.",
   },
 ];
+
+
+// Feature Data
+export const FeatureData = [ {
+    id: 1,
+    img: 'https://i.ibb.co/d0cQKdf/Free-Shipping.webp',
+    name: 'Free Shipping',
+    des: 'Deliver to door'
+  },
+  {
+    id: 2,
+    img: 'https://i.ibb.co/G52cjgw/Support.webp',
+    name: '24x7 Support',
+    des: 'in safe hands'
+  },
+  {
+    id: 3,
+    img: 'https://i.ibb.co/d6ny17k/Big-Saving.webp',
+    name: 'Big Saving',
+    des: 'at lowest price'
+  },
+  {
+    id: 4,
+    img: 'https://i.ibb.co/8sFJLtq/Money.webp',
+    name: 'Money Back',
+    des: 'Easy to return'
+  },
+  {
+    id: 5,
+    img: ' https://i.ibb.co/KFZVvjv/Online-Store.webp',
+    name: 'Online Store',
+    des: 'a huge branding'
+  },
+  {
+    id: 6,
+    img: 'https://i.ibb.co/2W744Xc/Award.webp',
+    name: 'Award Winner',
+    des: 'for best services'
+  },
+]
+
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,41 +1,165 @@
-import React, { Fragment } from 'react';
-import { Swiper, SwiperSlide } from "swiper/react";
+// import React, { Fragment } from "react";
+// import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+// // Import Swiper styles
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import "swiper/css/navigation";
 
-import "../../styles/components/banner.css";
+// import "../../styles/components/banner.css";
 
-// import required modules
-import { Pagination, Navigation } from "swiper";
+// // import required modules
+// import { Pagination, Autoplay, Navigation } from "swiper";
+// import { bannerData } from "../../StaticData/ProductData";
+
+// const Banner = () => {
+//   return (
+//     <Fragment>
+//       <Swiper
+//         slidesPerView={1}
+//         spaceBetween={30}
+//         loop={true}
+//         autoplay={{
+//           delay: 3000,
+//           disableOnInteraction: false,
+//         }}
+//         navigation={true}
+//         modules={[Pagination, Autoplay, Navigation]}
+//         className="mySwiper"
+//       >
+//         {bannerData.map((banner) => (
+//           <SwiperSlide key={banner.id}>
+//             <div class="card bg-base-100 shadow-2xl image-full">
+//               <figure>
+//                 <img
+//                   src={banner.img}
+//                   alt="Shoes"
+//                 />
+//               </figure>
+//               <div class="card-body">
+//                 <h2 class="card-title">{banner.name}</h2>
+//                 <p>{banner.res}</p>
+//                 <div class="card-actions justify-end">
+//                   <button class="btn btn-primary">Shop Now</button>
+//                 </div>
+//               </div>
+//             </div>
+//           </SwiperSlide>
+//         ))}
+//       </Swiper>
+//     </Fragment>
+//   );
+// };
+
+// export default Banner;
+
+import React from "react";
 
 const Banner = () => {
   return (
-    <Fragment>
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
-        loop={true}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="mySwiper"
+    <section>
+      <div
+        id="carouselDarkVariant"
+        class="carousel slide carousel-fade carousel-dark relative"
+        data-bs-ride="carousel"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-    </Fragment>
+        {/* <!-- Indicators --> */}
+        <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+          <button
+            data-bs-target="#carouselDarkVariant"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            data-bs-target="#carouselDarkVariant"
+            data-bs-slide-to="1"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            data-bs-target="#carouselDarkVariant"
+            data-bs-slide-to="2"
+            aria-label="Slide 1"
+          ></button>
+        </div>
+
+        {/* <!-- Inner --> */}
+        <div class="carousel-inner relative w-full overflow-hidden">
+          {/* <!-- Single item --> */}
+          <div class="carousel-item active relative float-left w-full">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(19).webp"
+              class="block w-full"
+              alt="Motorbike Smoke"
+            />
+            <div class="carousel-caption hidden md:block absolute text-center">
+              <h5 class="text-xl">First slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </div>
+          </div>
+
+          {/* <!-- Single item --> */}
+          <div class="carousel-item relative float-left w-full">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(35).webp"
+              class="block w-full"
+              alt="Mountaintop"
+            />
+            <div class="carousel-caption hidden md:block absolute text-center">
+              <h5 class="text-xl">Second slide label</h5>
+              <p>
+                Some representative placeholder content for the second slide.
+              </p>
+            </div>
+          </div>
+
+          {/* <!-- Single item --> */}
+          <div class="carousel-item relative float-left w-full">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(40).webp"
+              class="block w-full"
+              alt="Woman Reading a Book"
+            />
+            <div class="carousel-caption hidden md:block absolute text-center">
+              <h5 class="text-xl">Third slide label</h5>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* <!-- Inner --> */}
+
+        {/* <!-- Controls --> */}
+        <button
+          class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+          type="button"
+          data-bs-target="#carouselDarkVariant"
+          data-bs-slide="prev"
+        >
+          <span
+            class="carousel-control-prev-icon inline-block bg-no-repeat"
+            aria-hidden="true"
+          ></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+          type="button"
+          data-bs-target="#carouselDarkVariant"
+          data-bs-slide="next"
+        >
+          <span
+            class="carousel-control-next-icon inline-block bg-no-repeat"
+            aria-hidden="true"
+          ></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </section>
   );
 };
 
