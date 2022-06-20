04a575006db9671111859f7c3ef8b047798ea965
Muntasir Ahmed <muntasirahmed@Muntasirs-MacBook-Air.local>
2022-06-21 04:22:47 +0600

    navber design almost done

--- src/pages/shared/Navbar/Navbar.js
+++ src/pages/shared/Navbar/Navbar.js
@@ -1,7 +1,19 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
 import React, { useState } from "react";
-import { faArrowRightToBracket, faBagShopping, faEnvelope, faHeart, faPhone, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowRightToBracket,
+  faArrowAltCircleLeft,
+  faBagShopping,
+  faEnvelope,
+  faPhone,
+  faUser,
+  faSearch,
+  faHeart,
+  faHamburger,
+  faBars,
+  faClose,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import logo from "../../../assets/images/logo/logo.webp";
 import iconClose from "../../../assets/images/icon/close.png";
@@ -9,65 +21,80 @@ import iconHamburger from "../../../assets/images/icon/hamburger.png";
 import { Link } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
 import { menus } from "../../../StaticData/Menu";
+import loveIcon from "../../../assets/images/icon/loveIcon.png";
 
 const Navbar = () => {
-  let [open, steepen] = useState(false);
+  let [open, setOpen] = useState(false);
   const { logOut, user } = useAuth();
   return (
     <React.Fragment>
       <section>
         {/*phone, email and My Account top navbar */}
-        <div className="flex px-5 lg:px-10 justify-between  items-center">
+        <div className="flex text-black text-[12px] justify-between py-[9px] container items-center">
           <div className="hidden lg:inline-flex gap-x-5">
-            <div>
-              <FontAwesomeIcon icon={faPhone} /> <span>+8801998839126ldkfa</span>
+            <div className="flex items-center gap-2 font-sans">
+              <FontAwesomeIcon icon={faPhone} />{" "}
+              <span className="">+8801998839126</span>
             </div>
-            <div>
+            <div className="flex items-center gap-2 font-sans ">
               <FontAwesomeIcon icon={faEnvelope} />
-              <span className="hover:cursor-pointer hover:primary-300">fasion.house.italy@gmail.com</span>
+              <span className="hover:cursor-pointer hover:text-rose-600 ">
+                fasion.house.italy@gmail.com
+              </span>
             </div>
           </div>
           <div>
             <div className="dropdown hover:cursor-pointer dropdown-hover">
-              <h1 className="flex justify-between items-center">
+              <h1 className="flex justify-between items-center text-[12px] hover:text-rose-600">
                 My Account
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                >
                   <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
                 </svg>
               </h1>
-              <ul tabIndex="0" className="dropdown-content menu  shadow bg-base-100 rounded-md w-36">
-                <li>
-                  <a>
-                    <small>
-                      {" "}
-                      <FontAwesomeIcon icon={faArrowRightToBracket} /> Login
-                    </small>{" "}
-                  </a>
-                </li>
-                <li>
-                  <a>
-                    {" "}
-                    <small>
-                      <FontAwesomeIcon icon={faUser} />
-                      Create Account
-                    </small>
-                  </a>
-                </li>
-              </ul>
-              <ul tabIndex="0" className="dropdown-content menu p-3 shadow bg-base-100 rounded-box w-44">
+
+              <ul
+                tabIndex="0"
+                className="dropdown-content menu py-2 shadow  bg-base-100  rounded w-36"
+              >
                 {user.displayName ? (
                   <div>
                     <li>
-                      <button onClick={logOut}>Logout</button>
+                      <button
+                        className="flex text-[12px] gap-2 hover:text-rose-600"
+                        onClick={logOut}
+                      >
+                        {" "}
+                        <FontAwesomeIcon
+                          className=""
+                          icon={faArrowAltCircleLeft}
+                        />{" "}
+                        Logout
+                      </button>
                     </li>
                   </div>
                 ) : (
                   <div>
-                    <li>
-                      <Link to="/login">Login </Link>
+                    <li className=" ">
+                      <small className="flex text-[12px] gap-2 hover:text-rose-600">
+                        {" "}
+                        <FontAwesomeIcon
+                          className=""
+                          icon={faArrowRightToBracket}
+                        />{" "}
+                        <Link to="/login">Login </Link>
+                      </small>{" "}
                     </li>
                     <li>
-                      <Link to="/register">Create Account</Link>
+                      <small className="flex text-[12px] hover:text-rose-600 gap-2">
+                        <FontAwesomeIcon icon={faUser} />
+                        <Link to="/register">Create Account</Link>
+                      </small>
                     </li>
                   </div>
                 )}
@@ -77,93 +104,183 @@ const Navbar = () => {
         </div>
 
         {/* logo, catagories, search and cart icon option second navbar  */}
-        <div className="navbar bg-black px-5 my-3 py-10 lg:py-6 lg:px-10">
-          <div className="navbar-start">
-            <div className="dropdown">
-              <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                <select name="categories" id="categories" className="select">
-                  <option value="All Categories" disabled selected>
-                    All Categories
-                  </option>
-                  <option value="High Heels">High Heels</option>
-                  <option value="Flat Slippers">Flat Slippers</option>
-                  <option value="Hand Bags">Hand Bags</option>
-                  <option value="Modern Tops">Modern Tops</option>
-                  <option value="Fittops">Fittops</option>
-                  <option value="T-shirt">T-shirt</option>
-                  <option value="Jeans">Jeans</option>
-                </select>
+        <div className="bg-black py-4">
+          <div className="  container flex flex-col lg:flex-row items-center justify-between ">
+            <div className="">
+              <Link to="/home">
+                <div className="">
+                  {" "}
+                  <img className="h-[45px]" src={logo} alt="" />{" "}
+                </div>
+              </Link>
+            </div>
+            {/* <div className=" hidden lg:flex text-white">
+              <ul className="menu menu-horizontal p-0 ">
+                <div>
+                  <select
+                    name="categories"
+                    id="categories"
+                    className=" bg-[#3A3A3A] w-48 h-10 text-sm px-3 "
+                  >
+                    <option value="All Categories" disabled selected>
+                      All Categories
+                    </option>
+                    <option value="High Heels">High Heels</option>
+                    <option value="Flat Slippers">Flat Slippers</option>
+                    <option value="Hand Bags">Hand Bags</option>
+                    <option value="Modern Tops">Modern Tops</option>
+                    <option value="Fittops">Fittops</option>
+                    <option value="T-shirt">T-shirt</option>
+                    <option value="Jeans">Jeans</option>
+                  </select>
+                </div>
+                <div className="bg-[#3A3A3A] w-64 h-10 text-sm px-3 border-l-2 flex items-center justify-between">
+                  <input
+                    type="text"
+                    placeholder="Search.."
+                    className="bg-[#3A3A3A] focus:outline-none placeholder:text-white"
+                  />
+                  <span className=" text-white ">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5 flex items-center"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                      />
+                    </svg>
+                  </span>
+                </div>
               </ul>
-            </div>
+            </div> */}
+            <div className="flex items-center gap-x-10 mt-2">
+              <div className=" hidden  lg:flex text-white ">
+                <ul className="menu menu-horizontal p-0 ">
+                  <div>
+                    <select
+                      name="categories"
+                      id="categories"
+                      className=" bg-[#3A3A3A] xl:w-52 h-9 text-sm px-3 focus:outline-none"
+                    >
+                      <option value="All Categories" disabled selected>
+                        All Categories
+                      </option>
+                      <option value="High Heels">High Heels</option>
+                      <option value="Flat Slippers">Flat Slippers</option>
+                      <option value="Hand Bags">Hand Bags</option>
+                      <option value="Modern Tops">Modern Tops</option>
+                      <option value="Fittops">Fittops</option>
+                      <option value="T-shirt">T-shirt</option>
+                      <option value="Jeans">Jeans</option>
+                    </select>
+                  </div>
 
-            <Link to="/home">
-              <a className="btn btn-ghost normal-case text-xl">
-                {" "}
-                <img className="" src={logo} alt="" />{" "}
-              </a>
-            </Link>
-          </div>
-          <div className=" hidden lg:flex">
-            <ul className="menu menu-horizontal p-0">
-              <div>
-                <select name="categories" id="categories" className="select">
-                  <option value="All Categories" disabled selected>
-                    All Categories
-                  </option>
-                  <option value="High Heels">High Heels</option>
-                  <option value="Flat Slippers">Flat Slippers</option>
-                  <option value="Hand Bags">Hand Bags</option>
-                  <option value="Modern Tops">Modern Tops</option>
-                  <option value="Fittops">Fittops</option>
-                  <option value="T-shirt">T-shirt</option>
-                  <option value="Jeans">Jeans</option>
-                </select>
+                  <div className="bg-[#3A3A3A] xl:w-96 h-9 text-sm pl-2 2xl:mr-32 xl:mr-10 lg:mr-16 flex items-center justify-between">
+                    <input
+                      type="text"
+                      placeholder="Search.."
+                      className="bg-[#3A3A3A] focus:outline-none border-l h-full pl-3 border-stone-500 placeholder:text-white w-full"
+                    />
+                    <span className=" text-white cursor-pointer  h-9 px-3 flex items-center hover:bg-white hover:text-black  ">
+                      <FontAwesomeIcon icon={faSearch} className="text-lg " />
+                    </span>
+                  </div>
+                </ul>
               </div>
-              <input type="text" placeholder="Search.." className="input input-group-lg ml-3" />
-              <button className="btn text-white text-3xl ml-2 btn-ghost btn-circle">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-              </button>
-            </ul>
-          </div>
-          <div className="navbar-end px-5 gap-x-4">
-            <div className="text-3xl flex lg:hidden">
-              <button className="btn text-white text-3xl ml-2 btn-ghost btn-circle">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-              </button>
-            </div>
-            <div className="text-3xl ">
-              <FontAwesomeIcon icon={faHeart} className="text-white " />
-            </div>
-            <div className="indicator text-3xl flex items-center">
-              <span className="indicator-item badge badge-secondary hover:text-white">0</span>
-              <FontAwesomeIcon icon={faBagShopping} className="text-white hover:text-primary duration-150 cursor-pointer" />
+
+              <div>
+                <label for="my-drawer1">
+                  <img
+                    src={iconHamburger}
+                    className={"h-7 lg:hidden"}
+                    onClick={() => setOpen(true)}
+                    alt="icon"
+                  />
+                </label>
+              </div>
+              <div className="text-3xl flex lg:hidden">
+                <button className="btn text-white text-3xl  btn-ghost btn-circle">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-7"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    />
+                  </svg>
+                </button>
+              </div>
+
+              <div>
+                <span className="text-3xl">
+                  {" "}
+                  <FontAwesomeIcon
+                    icon={faHeart}
+                    className="text-white hover:text-primary duration-150 cursor-pointer"
+                  />
+                </span>
+              </div>
+              <div class="relative">
+                <span class="absolute top-[-10px]  rounded-full h-5 w-5 text-sm text-center bg-primary right-[-6px] text-white">
+                  0
+                </span>
+                <div class="dropdown dropdown-hover dropdown-end">
+                  <label tabindex="0" class="">
+                    <FontAwesomeIcon
+                      icon={faBagShopping}
+                      className="text-white p-1 text-3xl hover:text-primary duration-150 cursor-pointer"
+                    />
+                  </label>
+                  <ul
+                    tabindex="0"
+                    class="menu dropdown-content p-1 shadow font-semibold text-sm bg-base-100 w-64 lg:w-80 "
+                  >
+                    <li className="">
+                      <span className="flex items-center justify-center">
+                        Your Cart is currently empty!
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </div>
 
         {/* all catagories product name center third navbar */}
-        <nav className="flex items-center justify-center">
-          <img
-            src={open ? iconClose : iconHamburger}
-            className="md:hidden  fixed right-5 cursor-pointer z-20 top-10"
-            onClick={() => steepen(!open)}
-            alt="icon"
-          />
+        <nav className="">
+          <p
+            className="lg:hidden  absolute right-5 cursor-pointer z-20 top-10"
+            onClick={() => setOpen(false)}
+          >
+            <FontAwesomeIcon className="text-black w-full" icon={faClose} />
+          </p>
+          {/* <img src={open ? iconClose : iconHamburger} alt="icon" /> */}
           <ul
-            className={`bg-[#ffffffb0] backdrop-blur-md  md:pl-10 pr-28 md:static fixed duration-500 ease-linear top-0  md:h-auto h-screen z-10 py-[15px] ${
+            className={`bg-white lg:shadow-sm  backdrop-blur-md pl-10 pr-10 lg:static fixed duration-500 ease-linear  top-0 pt-16  lg:h-auto h-screen flex flex-col lg:flex-row justify-start lg:justify-center gap-5 lg:gap-8  z-10 py-[15px] ${
               !open ? "right-[-100%]" : "right-0"
             }`}
           >
             {menus.map((menu, index) => (
               <li
                 key={index}
-                className="md:inline-block text-zinc-800  md:ml-6 lg:ml-10 ml-5 border-b-2 border-transparent hover:border-primary duration-300"
+                className="lg:inline-block text-zinc-800   border-b-2 border-transparent hover:text-primary duration-300 "
               >
-                <a className=" cursor-pointer text-md inline-block py-3">{menu.name}</a>
+                <a className=" md:border-none border-b-[1px] border-gray-400  w-40 md:w-full cursor-pointer text-md block py-1">
+                  {menu.name}
+                </a>
               </li>
             ))}
           </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/tw-elements/dist/js/**/*.js"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
     extend: {
       colors: {
         primary: "#EE156A",
-        secondary: "#3A3A3A",
+        secondary: "#F70366",
+      },
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "980px",
+        xl: "1200px",
+        "2xl": "1500px",
       },
     },
   },
+
   plugins: [require("daisyui"), require("tw-elements/dist/plugin")],
 };
