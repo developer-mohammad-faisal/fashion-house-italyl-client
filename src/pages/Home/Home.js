c551236e36bba4ff8a5edf5dc3c4b1346a7538d6
alamincoders <alaminsbl502@gmail.com>
2022-06-22 01:26:39 +0600

    some bug fix and some design update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,13 +7,13 @@ const Home = () => {
   return (
     <main className="bg-[#F7F7F7] px-0 md:px-[45px] lg:px-[90px] py-10">
       <div className="w-full flex justify-center space-x-0 lg:space-x-5">
-        <section className="border-2  border-primary-600 w-[270px] h-full hidden lg:block">
+        <section className=" w-[270px] h-full hidden lg:block">
           <Left />
         </section>
-        <section className="border-2 border-primary-600 w-full h-full lg:w-[1024px]">
+        <section className=" w-full h-full lg:w-[1024px]">
           <Middle />
         </section>
-        <section className="border-2 border-primary-600 w-[270px] h-full hidden lg:block">
+        <section className=" w-[270px] h-full hidden lg:block">
           <Right />
         </section>
       </div>
