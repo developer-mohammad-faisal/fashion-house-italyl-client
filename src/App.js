80bc611611b6313eae459908f703d59044e7aafb
Muntasir Ahmed <muntasirahmed@Muntasirs-MacBook-Air.local>
2022-06-21 22:59:26 +0600

    brand info section done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/shared/Login/Login";
 import Navbar from "./pages/shared/Navbar/Navbar";
 import Register from "./pages/shared/Register/Register";
+import BrandInfo from "./components/shared/BrandInfo";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
           </Routes>
+          <BrandInfo/>
           <Footer></Footer>
         </BrowserRouter>
       </AuthProvider>
