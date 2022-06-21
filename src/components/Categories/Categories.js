88713b635ce0964e7bb90280e9e3d41ba43a92be
Your Name <you@example.com>
2022-06-21 20:41:13 +0600

    falsh saled section added

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -12,26 +12,12 @@ const Categories = () => {
               <ul className='divide-y divide-dashed'>
                 <li className='py-3 font-semibold' > <span >&#8226;</span> Long Top</li>
                 <li className='py-3 font-semibold' > <span >&#8226;</span> T-shirts</li>
-                <li className='py-3 font-semibold' > <span >&#8226;</span> Round Neck</li>
-                <li className='py-3 font-semibold' > <span >&#8226;</span> Sleeveless</li>
-                <li className='py-3 font-semibold' > <span >&#8226;</span> Full Hand</li>
                 <li className='py-3 font-semibold' > <span >&#8226;</span> Jeans</li>
-                <li className='py-3 font-semibold' > <span >&#8226;</span> Danbro</li>
-                <li className='py-3 font-semibold' > <span >&#8226;</span> Ben Martin</li>
-                <li className='py-3 font-semibold' > <span >&#8226;</span> Urbano</li>
                 <li className='py-3 font-semibold' > <span >&#8226;</span> Sandals</li>
                 <li className='py-3 font-semibold' > <span >&#8226;</span> High Heels</li>
-                <li className='py-3 font-semibold' > <span >&#8226;</span> Flat Boots</li>
-                <li className='py-3 font-semibold' > <span >&#8226;</span> Slippers</li>
-                <li className='py-3 font-semibold' > <span >&#8226;</span> Hand Bags</li>
                 <li className='py-3 font-semibold' > <span >&#8226;</span> Trendy Wear</li>
                 <li className='py-3 font-semibold' > <span >&#8226;</span> Slippers</li>
-                <li className='py-3 font-semibold' > <span >&#8226;</span> Lavie</li>
-                <li className='py-3 font-semibold' > <span >&#8226;</span> Pure</li>
-                <li className='py-3 font-semibold' > <span >&#8226;</span> United Colors</li>
                 <li className='py-3 font-semibold' > <span >&#8226;</span> Sandals</li>
-                <li className='py-3 font-semibold' > <span >&#8226;</span> Foot</li>
-                <li className='py-3 font-semibold' > <span >&#8226;</span> Boot</li>
               </ul>
 
              </div>
--- src/components/Home/Middle.js
+++ src/components/Home/Middle.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import Banner from "../Banner/Banner";
+import FlashSale from "../FlashSale/FlashSale";
 
 const Middle = () => {
   return (
     <Fragment>
       <div>
         {/* <Banner /> */}
+        <FlashSale/>
       </div>
     </Fragment>
   );
