487ccbfa3b446ab47bcefa71a5f020f1b495abea
Your Name <you@example.com>
2022-06-21 14:00:56 +0600

    categories added

--- src/StaticData/ProductData.js
+++ src/StaticData/ProductData.js
@@ -1,13 +1,3 @@
-export const ProductData = [
-  {
-    id: 1,
-    name: "T-Shirt",
-    price: 15,
-    desc: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit voluptatibus minus totam accusamus veniam molestias omnis error quidem. Quam, vitae.",
-    shop: "Gulshan",
-  },
-];
-
 // Banner Data
 export const bannerData = [
   {
