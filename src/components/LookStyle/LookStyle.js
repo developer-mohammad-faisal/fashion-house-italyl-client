d55608c317f790ff7ef16a7dbb5ab86cf42825c5
Muntasir Ahmed <muntasirahmed@Muntasirs-MacBook-Air.local>
2022-06-23 00:14:54 +0600

    look style banner done

--- src/components/LookStyle/LookStyle.js
+++ src/components/LookStyle/LookStyle.js
@@ -1,27 +1,33 @@
 import React, { Fragment } from "react";
-import '../../styles/components/LookStyle.css'
+import lookstyle from "../../assets/images/flashSale/lookStyle.webp";
 
 const LookStyle = () => {
   return (
-    <Fragment>
-      <section className="pt-10">
-        <section className="hood-sales-container text-white">
-         <div className="flex">
-
-          <div className="flex-1" ></div>
-
-         <div className="text-style flex-1 h-[280px] flex flex-col justify-center">
-            <h1>30% OFF</h1>
-            <h2>UNDER THE HOOD SALES</h2>
-            <p>
-              Contrary to popular belief, Lorem Ipsum is not simply random text.
+    <section className="h-[128px] md:h-[333px] mt-10">
+      <div
+        style={{ backgroundImage: `url(${lookstyle})` }}
+        className="bg-no-repeat bg-contain lg:bg-cover bg-center w-full   h-full"
+      >
+        <div className="flex h-full">
+          <div className="w-1/2 "></div>
+          <div className="pl-5 md:pl-20 w-1/2   flex items-start  justify-center h-[100%] flex-col ">
+            <h1 className=" text-[15px] md:text-2xl font-semibold text-white italic">
+              New
+            </h1>
+            <div className="text-lg flex flex-row lg:flex-col gap-3 md:text-4xl 2xl:text-5xl my-0 md:my-4 font-sans font-bold text-white italic">
+              LOOK & STYLE
+            </div>
+            <p className="text-white hidden md:block pr-5">
+              Nullam dictum felis eu pede mollis pretium. Integer tincidunt.
+              Cras.
             </p>
-            <button className="btn w-32" >Shop Now</button>
+            <button className="bg-[#058d82] hover:bg-primary transition-all duration-500 ease-in-out text-[14px] md:text-lg font-semibold mt-1 md:mt-5 text-white py-[3px] md:py-3 px-[8px] md:px-8">
+              Shop Now
+            </button>
           </div>
-         </div>
-        </section>
-      </section>
-    </Fragment>
+        </div>
+      </div>{" "}
+    </section>
   );
 };
 
