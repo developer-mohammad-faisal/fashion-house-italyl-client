191d2df2de56785f1361a12b03a89f548aafc96d
jannatul114 <jfmithila114@gmail.com>
2022-06-28 00:32:54 +0600

    first commit

--- src/components/Home/Products/Cloth.js
+++ src/components/Home/Products/Cloth.js
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 
 
 const Cloth = () => {
-const navigate = useNavigate()
+  const navigate = useNavigate()
 
   const handleNavigate = (id) => {
     navigate(`/clothDetails/${id}`)
@@ -45,14 +45,14 @@ const navigate = useNavigate()
                 <h3 className="text-2xl">{product.name.slice(0, 15)}...</h3>
               </div>
               <div className="price_rating">
-                <h2 className="text-xl font-sans">${product.price}</h2>
-                <div className="rating">
+                <h2 className="text-lg font-sans">€{product.price}</h2>
+                {/* <div className="rating">
                   <i className="fa fa-star" aria-hidden="true"></i>
                   <i className="fa fa-star" aria-hidden="true"></i>
                   <i className="fa fa-star" aria-hidden="true"></i>
                   <i className="fa fa-star" aria-hidden="true"></i>
                   <i className="fa fa-star" aria-hidden="true"></i>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
